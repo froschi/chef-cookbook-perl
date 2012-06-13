11e24b1b1d423bdb91bfe316395a7e7f85633dd2
Thorsten Fischer <thorsten@froschi.org>
2012-06-13 16:14:38 +0200

    Added depencies to recipe.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,13 @@
+include_recipe "perl-base"
+include_recipe "perl-modules"
+include_recipe "libbz2"
+include_recipe "libdb"
+include_recipe "libgdbm"
+
 packages = Array.new
 
 case node[:lsb][:codename]
-when "lucid"
-  packages |= %w/
-    perl
-  /
-when "precise"
+when "lucid", "precise"
   packages |= %w/
     perl
   /
